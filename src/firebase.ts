67291ce7c75bb6df2f9b04118fd82290b6b1d652
Bekforever13 <b3kforever@gmail.com>
2023-11-18 16:07:15 +0500

    added env

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBaxEVGB7FchgnzEODi_-fsw4yYeuiSyMs',
-  authDomain: 'portfolio-bde17.firebaseapp.com',
-  projectId: 'portfolio-bde17',
-  storageBucket: 'portfolio-bde17.appspot.com',
-  messagingSenderId: '116032724321',
-  appId: '1:116032724321:web:ec9c5313d869117b52f920',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
